636fa7b6488e8048efcd89cc03d6c7dc36fab486
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2024-12-29 00:43:37 +0700

    Create ProximitySensor.java

---
+++ app/src/main/java/com/voicechat/ProximitySensor.java
@@ -0,0 +1,44 @@
+package com.voicechat;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class ProximitySensor {
+
+    private SensorManager sensorManager;
+    private Sensor proximitySensor;
+    private ProximityListener listener;
+
+    public ProximitySensor(Context context) {
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+    }
+
+    public void startListening(ProximityListener listener) {
+        this.listener = listener;
+        sensorManager.registerListener(sensorEventListener, proximitySensor, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    public void stopListening() {
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+
+    private final SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+                listener.onProximityChanged(event.values[0]);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+    };
+
+    public interface ProximityListener {
+        void onProximityChanged(float distance);
+    }
+}
